13b96017c829fc8de0b1467462190d4acb4588a4
Harrison Healey <harrisonmhealey@gmail.com>
2024-11-22 12:51:09 -0500

    Start adding proper type definitions to Redux actions, starting with some user actions

--- webapp/channels/src/packages/mattermost-redux/src/action_types/index.ts
+++ webapp/channels/src/packages/mattermost-redux/src/action_types/index.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import type {BatchAction} from 'redux-batched-actions';
+
 import AdminTypes from './admin';
 import AppsTypes from './apps';
 import BotTypes from './bots';
@@ -28,6 +30,8 @@ import ScheduledPostTypes from './scheudled_posts';
 import SearchTypes from './search';
 import TeamTypes from './teams';
 import ThreadTypes from './threads';
+import type {AnyActionFrom} from './types';
+import type {UserAction} from './users';
 import UserTypes from './users';
 
 export {
@@ -60,3 +64,51 @@ export {
     ChannelBookmarkTypes,
     ScheduledPostTypes,
 };
+
+/**
+ * An InitAction is an empty action to initialize the Redux state, similar to the internal one used by Redux itself.
+ *
+ * It should only be used for testing.
+ */
+export interface InitAction {
+    type: '';
+}
+
+/**
+ * An ActionWithUndefinedStructure is any Redux action supported by mattermost-redux for which we haven't defined the
+ * required structure. All fields of it other than the the type itself will not be type checked.
+ */
+type ActionWithUndefinedStructure = AnyActionFrom<
+    typeof ErrorTypes &
+    typeof GeneralTypes &
+    typeof TeamTypes &
+    typeof ChannelTypes &
+    typeof PostTypes &
+    typeof FileTypes &
+    typeof PreferenceTypes &
+    typeof IntegrationTypes &
+    typeof EmojiTypes &
+    typeof AdminTypes &
+    typeof JobTypes &
+    typeof LimitsTypes &
+    typeof SearchTypes &
+    typeof RoleTypes &
+    typeof SchemeTypes &
+    typeof GroupTypes &
+    typeof BotTypes &
+    typeof PluginTypes &
+    typeof ChannelCategoryTypes &
+    typeof CloudTypes &
+    typeof AppsTypes &
+    typeof ThreadTypes &
+    typeof HostedCustomerTypes &
+    typeof DraftTypes &
+    typeof PlaybookType &
+    typeof ChannelBookmarkTypes &
+    typeof ScheduledPostTypes
+>;
+
+/**
+ * An MMReduxAction is any non-Thunk Redux action accepted by mattermost-redux.
+ */
+export type MMReduxAction = InitAction | BatchAction | UserAction | ActionWithUndefinedStructure;
